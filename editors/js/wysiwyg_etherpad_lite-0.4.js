47df70d328f4776277f5879fb6454090c2f7dafc
Constantin Jucovschi <jucovschi@gmail.com>
2011-12-06 05:35:08 +0100

    deprecated the need of drupal installation to share the same domain with the etherpad installation

--- editors/js/wysiwyg_etherpad_lite-0.4.js
+++ editors/js/wysiwyg_etherpad_lite-0.4.js
@@ -1,45 +1,73 @@
 (function($) {
 
 Drupal.wysiwyg.editor.init.etherpad_lite = function(settings) {
-	console.log(settings);
+
 }
 	
+
+function getPadContents(padid, callback, async) {
+    if (typeof(async)=="undefined")
+	async = true;
+    url = Drupal.settings.basePath+"localedit/get/";
+    data = {
+	"padid": padid 
+    };
+    var _call = callback;
+    $.ajax({
+	       "url":url,
+	       "type":"POST",
+	       "data":data,
+	       "async":async,
+	       "dataType":"json",
+	       "success" : function(data) {
+		   _call(data.content);
+	       }
+	   });
+}
+
+function setPadContents(padid, nid, content) {
+    url = Drupal.settings.basePath+"localedit/set/"+nid;
+    data = {
+	"padid": padid,
+	"content": content,
+    };
+    $.ajax({
+	       "url":url,
+	       "type":"POST",
+	       "data":data,
+	       "dataType":"json",
+	       "success" : function(data) {
+	       }
+	   });
+}
+
 /**
  * Attach this editor to a target element.
  */
 Drupal.wysiwyg.editor.attach.etherpad_lite = function(context, params, settings) {
   editorid = "#"+params.field;
-  console.log(params);
   newID = params.field+"-editor";
   $(editorid).after($("<div>").attr("id", params.field+"-editor").attr("class","pad"));
   obj = $(editorid).next();
   egid = $("#etherpad_gid").val();
   nid = $("#etherpad_nid").val();
+  padid =  egid+"$"+params.field;
   if (typeof(egid) != "undefined") {
-	  settings.egid = egid;
 	  $(obj).pad({
-			'padId': settings.egid+"$"+params.field,
+			'padId': padid,
 			'host':settings.host, 
 			'showChat':'true', 
 			'showControls':'true',
-		    'showLineNumbers'  : true,
-		    'userName'	 : settings.user,
+			 'showLineNumbers'  : true,
+			 'userName'	 : settings.user,
 			});
 	  if (typeof(nid)!="undefined" && nid.length>0) {
-		  $(obj).pad({'getContents': {
-			  "format": "txt",
-			  "callback": function(txt) {
-				  if (txt.length==1) {
-					  $(obj).pad({'host':settings.host, 
-   			  		  	'setContents':$(editorid).val(),
-   			  		  	"nid" : nid,
-   			  		  	'padId': settings.egid+"$"+params.field,
-   			  		  	});   				  
-				  }
-			  }
-		  }});
+	          getPadContents(padid, function(txt) {
+			if (txt.length==1) {
+			    setPadContents(padid, nid, $(editorid).val());
+			}			
+		  });
 	  }
-
 	  $(editorid).hide(); 
   }
 };
@@ -50,19 +78,15 @@ Drupal.wysiwyg.editor.attach.etherpad_lite = function(context, params, settings)
  * See Drupal.wysiwyg.editor.detach.none() for a full desciption of this hook.
  */
 Drupal.wysiwyg.editor.detach.etherpad_lite = function(context, params) {
+  egid = $("#etherpad_gid").val();
+  padid =  egid+"$"+params.field;
   editorid = "#"+params.field;
   newID = editorid+"-editor";
-  
-  $(newID).each(function(id, obj) {
-   	  $(obj).pad({'getContents': {
-   		  "format": "txt",
-   		  "callback": function(txt) {
-   			  $(editorid).val(txt);
-   	      }
-   	  }});
-   	  $(editorid).show();
-   	  $(obj).hide();
-   });
+  getPadContents(padid, function(content) {
+	$(editorid).val(content);
+	$(newID).hide();
+	$(editorid).show();
+  }, false);
 };
 
 })(jQuery);
